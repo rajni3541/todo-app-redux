ac6959fd5d6897829a697d9c66ae7ac5b3da6eb5
rajni <sainirajni292000@gmail.com>
2022-05-12 13:20:07 +0530

    commit

--- src/components/EditTodo/EditTodo.js
+++ src/components/EditTodo/EditTodo.js
@@ -1,8 +1,22 @@
-import { Button, Container, TextField } from "@mui/material";
+import { Button, Container, TextField, Modal, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 
 const EditTodo = () => {
     const { id } = useParams();
@@ -12,10 +26,20 @@ const EditTodo = () => {
         const { name, value } = event.target;
         setTodo({ ...todo, [name]: value });
     };
+    
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    
     const edit = async () => {
-    await axios.put(`http://localhost:8080/modify-todo/${id}`, todo).then(res => console.log(res));
-        navigate('/');
+        await axios.put(`http://localhost:8080/modify-todo/${id}`, todo).then(() => {
+            handleOpen();
+            setTimeout(() => {
+                navigate('/');
+            }, 1000);
+        });
     };
+
     return (<>
         <Container>
             <Box>
@@ -38,6 +62,23 @@ const EditTodo = () => {
                     variant="contained"
                     onClick={edit}>Edit</Button>
             </Box>
+
+            <Button onClick={handleOpen}>Notification</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        UPDATED TODO
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        TODO UPDATED SUCESSFULLY !!!!!!!!!!
+                    </Typography>
+                </Box>
+            </Modal>
         </Container>
     </>)
 };
